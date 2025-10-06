f68d88b2ccb3de73c04efd90ec92e6bf7dff0bcf
Giri karthick <girikarthi2526@gmail.com>
2025-10-06 09:02:17 +0000

    re_2FbQRhay_GEgzqx5cfwFMEZLiZbVCepRh use this api key da

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@
 // For more details on Resend's security model, please visit:
 // https://resend.com/docs/api-reference/authentication#security
 export const RESEND_API_KEY =
-  're_2vW7U6a5_CbgSMLYEXdCqdmgGEngz8Ev6';
+  're_2FbQRhay_GEgzqx5cfwFMEZLiZbVCepRh';
