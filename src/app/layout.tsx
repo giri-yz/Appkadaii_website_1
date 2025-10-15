4afd33ace418e6382f96b6e1afc921dab6505a09
Giri karthick <girikarthi2526@gmail.com>
2025-10-15 05:59:26 +0000

    same da

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,7 +104,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
           <Toaster />
